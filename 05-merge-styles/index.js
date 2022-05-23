7e83f8938bea00efbaea5178d3ed5fe3d73016cc
Aliaksandr Astrouski <aostrovsky87@gmail.com>
2022-05-23 15:08:52 +0300

    refactor: refactor 05-merge-styles solution

--- 05-merge-styles/index.js
+++ 05-merge-styles/index.js
@@ -4,23 +4,13 @@ const writeStream = fs.createWriteStream(path.join(__dirname, 'project-dist', 'b
 
 const sourceDirPath = path.join(__dirname, 'styles');
 
-function readStream(stream) {
-  return new Promise((resolve, reject) => {
-    let data = '';
-    stream.on('data', (chunk) => data += chunk);
-    stream.on('end', () => resolve(`${data}\n`));
-    stream.on('error', (err) => reject(err));
-  });
-}
-
 (async () => {
-  const files = await fs.promises.readdir(sourceDirPath, {withFileTypes: true});
-  files.forEach(async (file) => {
+  const files = await fs.promises.readdir(sourceDirPath, { withFileTypes: true });
+  files.filter((file) => file.isFile()).forEach((file) => {
     const filePath = path.join(sourceDirPath, file.name);
-    if (file.isFile() && (path.extname(filePath) === '.css')) {
-      const stream = fs.createReadStream(filePath, 'utf-8');
-      const fileData = await readStream(stream);
-      writeStream.write(fileData);
+    if (path.extname(filePath) === '.css') {
+      const readStream = fs.createReadStream(filePath, 'utf-8');
+      readStream.pipe(writeStream);
     }
   });
 })();
