9ad0fbd79b62a6a18880ffd0b77b828d048ade5e
Aliaksandr Astrouski <aostrovsky87@gmail.com>
2022-05-23 15:15:35 +0300

    refactor: refactor 03-files-in-folder solution

--- 03-files-in-folder/index.js
+++ 03-files-in-folder/index.js
@@ -1,19 +1,16 @@
 const path = require('path');
 const fs = require('fs/promises');
-const { stdout } = process;
 
 const dirPath = path.join(__dirname, 'secret-folder');
 
 (async () => {
-  const files = await fs.readdir(dirPath, {withFileTypes: true});
-  files.forEach(async (file) => {
+  const files = await fs.readdir(dirPath, { withFileTypes: true });
+  files.filter((file) => file.isFile()).forEach(async (file) => {
     const filePath = path.join(dirPath, file.name);
     const data = await fs.stat(filePath);
-    if (data.isFile()) {
-      const name = file.name.slice(0, file.name.lastIndexOf('.'));
-      const ext = path.extname(filePath).slice(path.extname(filePath).lastIndexOf('.') + 1);
-      const size = (data.size / 1024).toFixed(3) + 'kb';
-      stdout.write(`${name} - ${ext} - ${size}\n`);
-    }
+    const name = file.name.slice(0, file.name.lastIndexOf('.'));
+    const ext = path.extname(filePath).slice(1);
+    const size = (data.size / 1024).toFixed(3) + 'kb';
+    process.stdout.write(`${name} - ${ext} - ${size}\n`);
   });
 })();
